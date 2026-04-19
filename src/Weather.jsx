1901f09b5135f2f00f1161e291d7c35e324e9fc5
kylee-ingalls <kylee.ingalls@gmail.com>
2026-04-19 17:00:29 -0500

    Added basic styling

---
+++ src/Weather.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Monday 10:00</li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix d-flex">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="Cloudy"
+            />
+            <div>
+              <span className="temperature">15</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 20%</li>
+            <li>Humidity: 60%</li>
+            <li>Wind: 10 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
