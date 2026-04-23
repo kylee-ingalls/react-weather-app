8d07056dee2d97056ba6c0de6e549a6989863e91
kylee-ingalls <kylee.ingalls@gmail.com>
2026-04-22 23:20:28 -0500

    Added weather API

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,10 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      city: response.data.city,
+      time: response.data.time,
+      description: response.data.condition.description,
+      icon: response.data.condition.icon_url,
+      temperature: Math.round(response.data.temperature.current),
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+    });
+  }
+
+  function search() {
+    const apiKey = "cb60bbeo7bd602d062ff8d664eta0043";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function formatDate(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
+  }
+
   return (
     <div className="Weather">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="row">
           <div className="col-9">
             <input
@@ -12,6 +66,7 @@ export default function Weather() {
               placeholder="Enter a city..."
               className="form-control"
               autoFocus="on"
+              onChange={handleCityChange}
             />
           </div>
 
@@ -24,32 +79,36 @@ export default function Weather() {
           </div>
         </div>
       </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Monday 10:00</li>
-        <li>Cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <div className="clearfix d-flex">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-              alt="Cloudy"
-            />
-            <div>
-              <span className="temperature">15</span>
-              <span className="unit">°C</span>
+
+      {weatherData.ready && (
+        <>
+          <h1>{weatherData.city}</h1>
+
+          <ul>
+            <li>{formatDate(weatherData.time)}</li>
+            <li>{weatherData.description}</li>
+          </ul>
+
+          <div className="row">
+            <div className="col-6">
+              <div className="clearfix d-flex">
+                <img src={weatherData.icon} alt={weatherData.description} />
+                <div>
+                  <span className="temperature">{weatherData.temperature}</span>
+                  <span className="unit">°C</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-6">
+              <ul>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {weatherData.wind} km/h</li>
+              </ul>
             </div>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 20%</li>
-            <li>Humidity: 60%</li>
-            <li>Wind: 10 km/h</li>
-          </ul>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 }
